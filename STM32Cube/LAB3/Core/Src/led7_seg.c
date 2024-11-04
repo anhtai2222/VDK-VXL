e2ffc5ccd30717178abd102802373ab9d47bcb79
anhtai2222 <anhtaikolop@gmail.com>
2024-11-05 00:19:49 +0700

    fix-led7 not final

--- STM32Cube/LAB3/Core/Src/led7_seg.c
+++ STM32Cube/LAB3/Core/Src/led7_seg.c
@@ -15,24 +15,14 @@ void displaySEG(int bit, uint16_t type) {
         HAL_GPIO_WritePin(GPIOB, BIT_B[bit - 9], type);
     }
 }
+
 void display7SEG(int type, int num) {
-    uint8_t segmentPatterns[10][4] = {
-        {RESET, RESET, RESET, RESET}, // 0
-        {SET,   RESET, RESET, RESET}, // 1
-        {RESET, SET,   RESET, RESET}, // 2
-        {SET,   SET,   RESET, RESET}, // 3
-        {RESET, RESET, SET,   RESET}, // 4
-        {SET,   RESET, SET,   RESET}, // 5
-        {RESET, SET,   SET,   RESET}, // 6
-        {SET,   SET,   SET,   RESET}, // 7
-        {RESET, RESET, RESET, SET  }, // 8
-        {SET,   RESET, RESET, SET  }  // 9
-    };
 
     if (num < 0 || num > 9) {
         num = 0;
     }
+
     for (int i = 0; i < 4; i++) {
-        displaySEG(type * 4 + i, segmentPatterns[num][i]);
+        displaySEG(type * 4 + i, (num >> i) & 0x01);
     }
 }
